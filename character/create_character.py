6580a1b281f2fb1b16290249d02f3f1137281791
Alfrey-Chan <alfreychan@gmail.com>
2023-04-07 15:07:36 -0700

    Create character function and init file for package

---
+++ character/create_character.py
@@ -0,0 +1,23 @@
+"""
+Alfrey Chan
+A01340049
+"""
+
+
+def create_character():
+    character = {
+        'X-coordinate': 0,
+        'Y-coordinate': 0,
+        'HP': 100,
+        'skills': {'strength': 1, 'agility': 1, 'luck': 1},
+        'inventory': []
+    }
+    return character
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
