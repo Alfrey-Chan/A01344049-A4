0d3f855a46c8c163cbf1a3526f2ed81225c8f522
Alfrey-Chan <alfreychan@gmail.com>
2023-04-10 03:24:42 -0700

    Add functions for combat

--- combat/combat.py
+++ combat/combat.py
@@ -5,6 +5,71 @@ A01344049
 import random
 
 
+def chance_game(numbers: list):
+    if random.choice(numbers) == 1:
+        return True
+    return False
+
+
+def flee(agility: int) -> bool:
+    avoid_damage_chance = {
+        1: chance_game(list(range(10))),
+        2: chance_game(list(range(3))),
+        3: chance_game(list(range(2))),
+    }
+    flee_successful = avoid_damage_chance[agility]
+    if flee_successful:
+        print("Thanks to your shiftiness, you managed to flee with no injuries.")
+        return False
+    else:
+        print("You find yourself too slow to escape from the enemy. You are forced to fight.")
+        return True
+
+
+def is_jesus(character):
+    if character['skills']['luck'] == 3:
+        win_game = chance_game([1, 1, 1, 1, 0])
+        if win_game:
+            print("""You feel the lucky charms soaring through your veins!\nYour HP has been restored and your 
+            radiation levels have been reduced to zero!""")
+            character['HP'] = 100
+            character['radiation'] = 0
+            print(character)
+
+
+def attack(character_stats, foe_damage):
+    strength_level = character_stats['skills']['strength']
+    damage_reduction = {
+        1: 1,
+        2: 3,
+        3: 2
+    }
+    print("You've defeated the opponent! But you've taken some damage in return.")
+    character_stats['HP'] = character_stats['HP'] - round(foe_damage/damage_reduction[strength_level])
+
+
+def battle(character, foe):
+    is_jesus(character)
+    while True:
+        try:
+            flee_or_attack = int(input("Would you like to battle or take your chances and flee? \n"
+                                       "\tChoose one:\n\t'1' to battle\n\t'2' to flee\n").strip())
+            if flee_or_attack not in [1, 2]:
+                raise ValueError
+
+            if flee_or_attack == 1:
+                attack(character, foe[1]['damage'])
+            else:
+                flee_unsuccessful = flee(character['skills']['agility'])
+                if flee_unsuccessful:
+                    attack(character, foe[1]['damage'])
+            break
+        except ValueError:
+            print("Enter a valid input!")
+
+    return character
+
+
 def check_for_enemies(character):
     def spawn_special_foe(location: tuple) -> tuple:
         special_spawns = {
